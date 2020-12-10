8f6d132bd6c00d8aed9a85fd196a6c172c2d629b
Mandeep Punia <Mandeeppunia08@gmail.com>
2020-12-10 16:46:30 +0530

    sketch.js updated

--- sketch.js
+++ sketch.js
@@ -1,21 +1,24 @@
-var x = 0;
-var y = 0;
-var speedx = 3;
-var speedy = 3;
+var x = 0; //x coordinate of the ball at any instant
+var y = 0; // y coordinate of the ball at any instant
+var speedx = 3; //speed of the ball along the x axis
+var speedy = 3; // speed of the ball along the y axis
 let football;
 let wall;
 
 function setup() {
+
     createCanvas(windowWidth, windowHeight);
     background(300);
 }
 
 function preload() {
+    // A method which will load all the stuff before hand i.e.,before running of the program it will load the stuff.
     football = loadImage('images/football.png');
     wall = loadImage('images/bricks.png');
 }
 
 function draw() {
+    //logic part of the project 
     background(0);
     image(wall, 0, 0, windowWidth, windowHeight);
     stroke(255);
